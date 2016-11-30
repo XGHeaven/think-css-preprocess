551bd95a3549a3accae95449840cd9088c6dff4e
XGHeaven <xgheaven@gmail.com>
2016-11-30 11:44:43 +0800

    finish base function
    support path config, (string or path)
    update package.json
    update travis.yml

--- src/index.js
+++ src/index.js
@@ -1,5 +1,103 @@
 'use strict';
 
-export default class {
-  
+import accord from 'accord';
+import path from 'path';
+import fs from 'fs';
+
+const writeFile = think.promisify(fs.writeFile, fs);
+
+export default class extends think.middleware.base {
+  init(...args) {
+    super.init(...args);
+    this._compilerCaches = {};
+    this._compilers = {
+      stylus: '.styl',
+      less: '.less',
+      sass: '.sass'
+    }
+  }
+
+  getCompiler(name) {
+    if (!this._compilerCaches[name]) {
+      this._compilerCaches[name] = accord.load(name);
+    }
+    return this._compilerCaches[name];
+  }
+
+  splitPath(p) {
+    p = p.split(path.sep);
+    if (p[0] === '') p.shift();
+    if (p[p.length-1] === '') p.pop();
+    return p;
+  }
+
+  joinPath(p1, p2) {
+    p2.splice(0, p1.length);
+    return '/static/' + p1.join(path.sep) + path.sep + p2.join(path.sep);
+  }
+
+  arrayHasPrefix(prefix, array) {
+    for (let i=0; i<prefix.length; i++) {
+      if (prefix[i] !== array[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  getRelativeFilePath(ext) {
+    let config = this.config('css_preprocess.path');
+    let url = this.http.url;
+    let dirs = this.splitPath(url);
+    dirs.shift(); // remove 'static'
+    if (think.isString(config)) {
+      let nDirs = this.splitPath(config);
+      if (nDirs.length < dirs.length) {
+        url = this.joinPath(nDirs, dirs);
+      }
+    } else if (think.isArray(config) && config.length === 2) {
+      let nDirs = this.splitPath(config[0]);
+      let rDirs = this.splitPath(config[1]);
+      if (this.arrayHasPrefix(rDirs, dirs)) {
+        dirs.splice(0, rDirs.length);
+        url = '/static/' + nDirs.join(path.sep) + path.sep + dirs.join(path.sep);
+      }
+    }
+    let pathInfo = path.parse(path.join(think.RESOURCE_PATH, url));
+    pathInfo.ext = ext;
+    delete pathInfo.base;
+    return path.format(pathInfo);
+  }
+
+  async run() {
+    let url = this.http.url;
+    if (path.parse(url).ext !== '.css') {
+      // only compiler css file
+      return;
+    }
+    let oldFilePath = path.join(think.RESOURCE_PATH, url);
+    for (let name in this._compilers) {
+      let newFilePath = this.getRelativeFilePath(this._compilers[name]);
+      if (
+        !think.isFile(newFilePath) ||
+        (think.env === 'production' && !this.newThanOld(newFilePath))) {
+        continue;
+      }
+      try {
+        let compiler = this.getCompiler(name);
+        let output = await new Promise((resolve, reject) => {
+          return compiler
+          .renderFile(newFilePath)
+          .catch(err => reject(err))
+          .done(res => resolve(res.result))
+        });
+        await writeFile(oldFilePath, output);
+        return;
+      } catch (e) {
+        this.http.status(500).end(e.message);
+        if (think.env === 'testing') return;
+        return think.prevent();
+      }
+    }
+  }
 }
--- test/index.js
+++ test/index.js
@@ -3,54 +3,103 @@ var path = require('path');
 var fs = require('fs');
 var http = require('http');
 
+var rimraf = require('rimraf');
+
 var thinkjs = require('thinkjs');
-var instance = new thinkjs();
+var instance = new thinkjs({
+  RESOURCE_PATH: __dirname + '/resource/'
+});
 instance.load();
+think.env = 'testing';
 
+var Class = require('../lib/index.js').default;
 
-var Class = require('../lib/index.js');
+var getHttp = function(url, options){
+  var req = new http.IncomingMessage();
+  req.headers = {
+    'host': 'www.thinkjs.org',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_0) AppleWebKit',
+  };
+  req.method = 'GET';
+  req.httpVersion = '1.1';
+  req.url = url;
+  var res = new http.ServerResponse(req);
+  res.write = function(){
+    return true;
+  };
 
+  return think.http(req, res).then(function(http){
+    if(options){
+      for(var key in options){
+        http[key] = options[key];
+      }
+    }
+    return http;
+  })
+};
 
-// var getHttp = function(options){
-//   var req = new http.IncomingMessage();
-//   req.headers = { 
-//     'host': 'www.thinkjs.org',
-//     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-//     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_0) AppleWebKit',
-//   };
-//   req.method = 'GET';
-//   req.httpVersion = '1.1';
-//   req.url = '/index/index';
-//   var res = new http.ServerResponse(req);
-//   res.write = function(){
-//     return true;
-//   }
-
-//   return think.http(req, res).then(function(http){
-//     if(options){
-//       for(var key in options){
-//         http[key] = options[key];
-//       }
-//     }
-//     return http;
-//   })
-// }
-
-// var execMiddleware = function(options){
-//   return getHttp(options).then(function(http){
-//     var instance = new Class(http);
-//     return instance.run();
-//   })
-// }
+var execMiddleware = function(url, options){
+  return getHttp(url, options).then(function(http){
+    var instance = new Class(http);
+    return instance.run().then(() => http)
+  })
+};
 
 
 describe('think-css-preprocess', function(){
-  it('test sync', function(){
-
-  })
-  it('test async', function(done){
+  it('compile stylus', function(done){
     Promise.resolve().then(function(){
-      done();
-    })
-  })
-})
+      execMiddleware('/static/stylus/index.css').then(function(http) {
+        assert.ok(think.isFile(think.RESOURCE_PATH + 'static/stylus/index.css'));
+        done();
+      }).catch(err => done(err));
+    });
+  });
+
+  afterEach(function() {
+    rimraf.sync(think.RESOURCE_PATH + 'static/**/*.css');
+  });
+});
+
+describe('compile css file in different path through set config', function() {
+  it('path = string', function(done) {
+    Promise.resolve().then(function() {
+      execMiddleware('/static/target/index.css', {
+        _config: {
+          css_preprocess: {
+            path: 'source1'
+          }
+        }
+      }).then(function(http) {
+        assert.ok(
+          think.isFile(think.RESOURCE_PATH + 'static/target/index.css'),
+          'file not create in static/target'
+        );
+        done()
+      }).catch(err => done(err));
+    });
+  });
+
+  it('path = array', function(done) {
+    Promise.resolve().then(function() {
+      execMiddleware('/static/target/target1/index.css', {
+        _config: {
+          css_preprocess: {
+            path: ['source1', 'target/target1']
+          }
+        }
+      }).then(function(http) {
+        assert.ok(
+          think.isFile(think.RESOURCE_PATH + 'static/target/target1/index.css'),
+          'file not create in static/target/target1'
+        );
+        done();
+      }).catch(err => done(err));
+    });
+  });
+
+  afterEach(function() {
+    rimraf.sync(think.RESOURCE_PATH + 'static/**/*.css');
+  });
+});
